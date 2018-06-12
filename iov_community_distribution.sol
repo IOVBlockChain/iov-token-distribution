853149d2784447d38c3c196741207313eaf34e7e
pinkecoin <mrjzhao@yahoo.com.hk>
2018-06-13 01:19:58 +0800

    update the community distribution solidity

--- iov_community_distribution.sol
+++ iov_community_distribution.sol
@@ -4,21 +4,11 @@ import './iov_token.sol';
 
 contract IOVCommunityDistribution is DSAuth, DSMath {
     IOVToken  public  IOV;                           // The IOV token itself
-    uint256   public  TOTAL_COMMUNITY_SUPPLY;        // Total IOV amount for community distribution
-    uint256   public  AVAILABLE_COMMUNITY_SUPPLY;    // Remain tokens for community distribution
-    mapping (address => uint256) public  _userDistributionAmount; // the distribution amount for each user
 
     mapping (address => bool) public isCommunityAdmin;  // community Admin accounts
 
     event LogAddCommunityAdmin(address whoAdded, address newAdmin);
     event LogRemoveCommunityAdmin(address whoRemoved, address admin);
-
-    constructor(
-        uint256  _distributionSupply
-    ) public {
-        TOTAL_COMMUNITY_SUPPLY       = _distributionSupply;  // 30% for community distribution
-        AVAILABLE_COMMUNITY_SUPPLY   = _distributionSupply - 5*10**6*10**8; // 
-    }
 
     function initialize(IOVToken iov) public auth {
         assert(address(IOV) == address(0));
@@ -53,10 +43,6 @@ contract IOVCommunityDistribution is DSAuth, DSMath {
         uint256 i = 0;
         while (i < dests.length) {
             IOV.push(dests[i], values[i]);
-
-            _userDistributionAmount[dests[i]] = add(_userDistributionAmount[dests[i]], values[i]);
-            AVAILABLE_COMMUNITY_SUPPLY = sub(AVAILABLE_COMMUNITY_SUPPLY, values[i]);
-            
             i += 1;
         }
         return(i);
